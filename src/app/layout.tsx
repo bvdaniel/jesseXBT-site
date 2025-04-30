23a7cef1e89c1152f9306413b3dc87d0641aa7ba
Daniel Beltran <beltranvarasdaniel@gmail.com>
2025-04-30 18:29:07 -0400

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "jesseXBT",
-  description: "jesseXBT - LFB",
+  description: "Digital clone of @jessepollak. here to help you build.",
+  metadataBase: new URL('https://jessexbt.live'),
+  openGraph: {
+    title: 'jesseXBT',
+    description: 'Digital clone of @jessepollak. here to help you build.',
+    url: 'https://www.jessexbt.live',
+    siteName: 'jesseXBT',
+    images: [
+      {
+        url: '/assets/jesseXBT-preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'jesseXBT Preview',
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'jesseXBT',
+    description: 'Digital clone of @jessepollak. here to help you build.',
+    creator: '@jessepollak',
+    images: ['/assets/jesseXBT-preview.png'],
+  },
+  icons: {
+    icon: '/assets/favicon.ico',
+    shortcut: '/assets/favicon-16x16.png',
+    apple: '/assets/apple-touch-icon.png',
+  },
+  manifest: '/assets/site.webmanifest',
 };
 
 export default function RootLayout({
